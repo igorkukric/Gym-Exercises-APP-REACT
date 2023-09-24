88af359ebcfcddcf7f14dbafaa749468facdaec7
Igor Kukric <igorkukric@gmail.com>
2023-09-24 17:54:30 +0200

    add ExerciseCard

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Stack, Typography } from '@mui/material'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <Link to={`/exercise/${exercise.id}`}>
-
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#ffffff",
+            background: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.targetMuscle}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight="bold"
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize="22px"
+      >
+        {exercise.name}
+      </Typography>
     </Link>
-  )
-}
+  );
+};
 
-export default ExerciseCard
+export default ExerciseCard;
