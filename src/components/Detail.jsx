1f5590caae8fdeae864941a734c27f3157348303
Igor Kukric <igorkukric@gmail.com>
2023-09-25 00:11:45 +0200

    update Detail and ExerciseDetail components

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Typography, Stack, Button, capitalize } from "@mui/material";
 
-const Detail = () => {
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipmment } = exerciseDetail;
+
   return (
-    <div>Detail</div>
-  )
-}
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography variant="h3" textTransform='capitalize'>{name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong. {name} {``}
+          is one of the best exercises to target your {target}. It will help you
+          improve your mood and gain energy.
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Detail
+export default Detail;
