0c2af4697a84c3d26fa3ff227b6edadf2a5a3464
Igor Kukric <igorkukric@gmail.com>
2023-09-25 02:18:36 +0200

    update similarexercises

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -4,6 +4,7 @@ import { VisibilityContext, ScrollMenu } from "react-horizontal-scrolling-menu";
 
 
 import BodyPart from "./BodyPart";
+import ExerciseCard from './ExerciseCard'
 
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
@@ -29,7 +30,7 @@ const RightArrow = () => {
 };
 
 
-const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => {
+const HorizontalScrollbar = ({ data, setBodyPart, bodyPart, isBodyParts }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
@@ -39,7 +40,9 @@ const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => {
           title={item.id || item}
           m="0 40px"
         >
-          <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart}  />
+         {isBodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart}  />
+         : <ExerciseCard exercise={item} />
+        }
         </Box>
       ))}
     </ScrollMenu>
